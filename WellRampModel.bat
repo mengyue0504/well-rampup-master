89592985d359733ac2df45166b092fc6a7ad7c3b
Mengyue-Yang-ws <mengyue.yang@woodside.com>
2024-12-17 14:43:09 -0600

    Delete WellRampModel.bat

--- WellRampModel.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Run the first Python script (WellRampModel.py)
-echo Running WellRampModel.py...
-python "C:\ShenziRampup\WellRampModel.py"
-if %errorlevel% neq 0 (
-    echo WellRampModel.py failed. Exiting...
-    exit /b %errorlevel%
-)
